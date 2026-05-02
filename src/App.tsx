f541fcfcdfeaa7e67c85c37b394d504b2db09954
awais-ansarii <awaisansari1502@gmail.com>
2026-05-02 14:53:52 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-screen h-screen bg-black text-white p-4'>hi</div>
+  )
+}
+
+export default App
