34dd3d2e831a4518b6e09e57e4058a536eb39c2d
awais-ansarii <awaisansari1502@gmail.com>
2026-05-02 15:30:15 +0530

    state added for lint check

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <div className='w-screen h-screen bg-black text-white p-4'>hi</div>
