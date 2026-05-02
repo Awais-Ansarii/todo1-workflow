15e8ebed012992bc367a77811d362e62b1356618
awais-ansarii <awaisansari1502@gmail.com>
2026-05-02 15:37:01 +0530

    counter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import {  useState } from 'react'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
-    <div className='w-screen h-screen bg-black text-white p-4'>hi</div>
+    <div className='w-screen h-screen bg-black text-white p-4'>
+
+      <div className='flex flex-col gap-4 items-center justify-center'>
+
+      <p>count = {count}</p>
+      <button onClick={()=>setCount((prev)=>prev+1)}>increase count</button>
+    </div>
+      </div>
   )
 }
 
